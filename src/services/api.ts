e124e11b8d21c06fa35cedbebf485ffafbe4dca2
Guilherme Ferreira <guilherme.ferreira@estudante.ifb.edu.br>
2022-02-22 00:56:45 -0300

    refactor: remove redundant code

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,39 +7,6 @@ export const api = axios.create({
 
 const baseUrl = "http://localhost:3333/api";
 
-// creat
-export async function createRow(row: Process) {
-  try {
-    const response = await axios.post(`${baseUrl}/spreadsheet`, {
-      value: {
-        "Número do Processo": row.numeroProcesso,
-        "Data de protocolo do pedido": row.dataProtocoloPedido,
-        "Porto Organizado": row.portoOrganizado,
-        "Contrato de Arrendamento": row.contratoArrendamento,
-        Arrendatário: row.arrendatario,
-        "Valor do investimento proposto": row.valorInvestimentoProposto,
-        "Perfil de carga": row.perfilCarga,
-        "Tipo de carga": row.tipoCarga,
-        "Análise da GPO": row.analiseGPO,
-        OBJETO: row.objeto,
-        OBSERVAÇÕES: row.observacoes,
-        TÉCNICO: row.tecnico,
-        "Andamento GPO": row.andamentoGPO,
-        "Início da Análise - GPO": row.inicioAnaliseGPO,
-        "Término da Análise - GPO": row.terminoAnaliseGPO,
-        "Prazo de Análise": row.prazoAnalise,
-        SITUAÇÃO: row.situacao,
-        "Manifestação da ANTAQ (Diretoria/SOG)": row.manifestacaoANTAQ,
-        "Deliberação da diretoria (e/ou declaração técnica SOG)":
-          row.deliberacaoDiretoria,
-      },
-    });
-    return response;
-  } catch (error) {
-    throw new Error(error);
-  }
-}
-
 // read
 export async function getSpreadsheet() {
   try {
