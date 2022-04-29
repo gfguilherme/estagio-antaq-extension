55f2d0851780ef1a5c650d8754e28d1a524fd5ec
Guilherme Ferreira <guilherme.xgfx@gmail.com>
2022-04-29 13:30:45 -0300

    refactor: change typescript behavior

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,24 @@ const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
+const fileExtensions = [
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'eot',
+  'otf',
+  'svg',
+  'ttf',
+  'woff',
+  'woff2',
+];
 
 module.exports = function (env, argv) {
   return {
+    context: __dirname,
     mode: env.production ? 'production' : 'development',
     devtool: env.production ? 'source-map' : 'cheap-source-map',
     optimization: env.production
@@ -24,12 +37,21 @@ module.exports = function (env, argv) {
 
     entry: {
       background: path.resolve(__dirname, 'src/pages/Background/index.ts'),
-      buttonsScript: path.resolve(__dirname, 'src/pages/Content/buttonsScript.tsx'),
-      dialogScript: path.resolve(__dirname, 'src/pages/Content/dialogScript.tsx'),
+      buttonsScript: path.resolve(
+        __dirname,
+        'src/pages/Content/buttonsScript.tsx'
+      ),
+      dialogScript: path.resolve(
+        __dirname,
+        'src/pages/Content/dialogScript.tsx'
+      ),
       gridScript: path.resolve(__dirname, 'src/pages/Content/gridScript.tsx'),
       options: path.resolve(__dirname, 'src/pages/Options/index.tsx'),
       popup: path.resolve(__dirname, 'src/pages/Popup/popup.tsx'),
-      snackbarScript: path.resolve(__dirname, 'src/pages/Content/snackbarScript.tsx'),
+      snackbarScript: path.resolve(
+        __dirname,
+        'src/pages/Content/snackbarScript.tsx'
+      ),
     },
 
     output: {
@@ -42,28 +64,16 @@ module.exports = function (env, argv) {
     module: {
       rules: [
         {
-          test: /\.html$/,
-          loader: 'html-loader',
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'ts-loader',
-              options: {
-                transpileOnly: true,
-              },
-            },
-            {
-              loader: 'source-map-loader',
-            },
-          ],
-        },
-        {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            transpileOnly: true,
+          },
         },
         {
           test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
@@ -79,6 +89,7 @@ module.exports = function (env, argv) {
     },
 
     plugins: [
+      new ForkTsCheckerWebpackPlugin(),
       new CleanWebpackPlugin({ verbose: false }),
       new webpack.ProgressPlugin(),
 
